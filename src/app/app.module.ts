fa39924ba8e8caeea908733969fcbb6e7e8d8afd
Stephen Kalletta <Stephen.Kalletta@gmail.com>
2017-10-03 01:01:39 +0100

    Editting first component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import {MdToolbarModule} from '@angular/material';
+import {FlexLayoutModule} from '@angular/flex-layout';
+
+import 'hammerjs';
 
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpModule,
+    MdToolbarModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
